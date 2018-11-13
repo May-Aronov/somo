f5832f8916f60799f507a1189e8c8cbd24dc8ff4
shirhen194 <michalbenbasat1@gmail.com>
2018-11-13 17:34:34 +0200

    with db

---
+++ server/data-access/Hashtag.js
@@ -0,0 +1,10 @@
+const connection=require('./da') 
+const Sequelize = require('sequelize');
+
+Hashtag = connection.define("hashtag", {
+    name: { type: Sequelize.STRING }
+})
+
+// Hashtag.sync({})
+
+module.exports =(Hashtag);
---
+++ server/data-access/createDB.js
@@ -0,0 +1,15 @@
+const connection = require('./da')
+// const Sequelize = require('sequelize');
+const Hashtag = require('./Hashtag')
+const Product = require('./Product')
+const User = require('./User')
+const Review = require('./Review')
+
+
+
+
+
+
+
+
+// module.exports = ();
