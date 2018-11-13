9793417af9995db57aaf2d468fff0301b5a8c782
ofrizadok <ofrizz@walla.com>
2018-11-13 18:40:55 +0200

    componets

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="Linkim">
+            <ul>
+              <li><Link to="/addReview">AddReview</Link></li>
+              <li><Link to="/search">Search</Link></li>
+
+
+            </ul>
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/addReview.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class addReview extends Component {
+  render() {
+    return (
+      <div className="Search">
+        
+      </div>
+    );
+  }
+}
+
+export default addReview;
