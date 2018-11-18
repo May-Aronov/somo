efe54a63fa3a8e1814d6f08acab2ea5d1ad7564e
Nitzancohen <orhirak@gmail.com>
2018-11-18 13:56:20 +0200

    sada

--- server/apis/usersApi.js
+++ server/apis/usersApi.js
@@ -8,35 +8,35 @@ const Review = require('../data-access/Review')
 
 
 
-router.post('/newreview/:username',async(req, res) => {
+router.post('/newreview/:username', async (req, res) => {
     let reqData = req.body;//{ username: "", productType: "",productname: "",reviewText: ""}
-   console.log(reqData)
- await Product
-  .findOrCreate({ where: {type: reqData.productType, name: reqData.productName , imgurl:reqData.productImgUrl , urlid:reqData.productUrlId} })
-  .spread(async(user, created) => {
- let product= await user.get({plain: true})
- console.log(product)
- let review= await Review.create({text:reqData.reviewText,productId:product.id})
- console.log(review)
- res.status(201).send(review)
-  })
+    console.log(reqData)
+    await Product
+        .findOrCreate({ where: { type: reqData.productType, name: reqData.productName, imgurl: reqData.productImgUrl, urlid: reqData.productUrlId } })
+        .spread(async (user, created) => {
+            let product = await user.get({ plain: true })
+            console.log(product)
+            let review = await Review.create({ text: reqData.reviewText, productId: product.id })
+            console.log(review)
+            res.status(201).send(review)
+        })
 
-  res.status(500).send(err);
+    res.status(500).send(err);
 })
- 
+
 
 router.get('/search/:SearchText', async (req, res) => {
     let searchtext = req.params.SearchText;
     Product.findAll({
-        where:{name:searchtext},
+        where: { name: searchtext },
         include: [Review]
-    }).then(product=>{
- 
-        res.status(201).send(product)  
+    }).then(product => {
+
+        res.status(201).send(product)
     })
 
 })
- 
+
 // model:Review, include: [{
 //     model:  User ,
 //     where: { id: Sequelize.col('review.userId') }
