11c7f668f32a3474f4dad2f718b0e8d989b71522
shirhen194 <michalbenbasat1@gmail.com>
2018-11-21 18:19:34 +0200

    heroko1

---
+++ build/precache-manifest.444bc3688719f8aa6409eadff01b3d58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b285c4f8cca6555a78df6edbcdc71ba4",
+    "url": "/static/media/nice.b285c4f8.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f54280f209e24bbffea2",
+    "url": "/static/js/main.f54280f2.chunk.js"
+  },
+  {
+    "revision": "1bc2920080f3caf0089b",
+    "url": "/static/js/1.1bc29200.chunk.js"
+  },
+  {
+    "revision": "f54280f209e24bbffea2",
+    "url": "/static/css/main.9df95787.chunk.css"
+  },
+  {
+    "revision": "1bc2920080f3caf0089b",
+    "url": "/static/css/1.2663aae5.chunk.css"
+  },
+  {
+    "revision": "3e72606d38e7420ae4996b0dfb4f5afa",
+    "url": "/index.html"
+  }
+];
